c83a13e0aefce312d7470c7a23bb225a131e0fb3
Suli <sulihu@bu.edu>
2020-03-08 14:00:30 -0400

    aws7

--- rest_run.py
+++ rest_run.py
@@ -22,8 +22,6 @@ def my_form_post():
     mtw4.start()
     zipFolder = zipfile.ZipFile('videos.zip','w', zipfile.ZIP_DEFLATED)
     for root, directs, files in os.walk('./video'):
-        # if os.path.exists('./video/.DS_Store'):
-        #   os.remove('./video/.DS_Store')
         for f in files:
             print(f)
             if str(f) == '.DS_Store':
@@ -31,39 +29,10 @@ def my_form_post():
             zipFolder.write('./video/' + str(f))
     zipFolder.close()
     print('zip close')
-    # for root, directs, files in os.walk('./video'):
-    #   for f in files:
-    #       print(f)
-    #       os.remove(f)
     os.system("rm video/*")
     return send_file('videos.zip', mimetype ='zip', attachment_filename = 'videos.zip', as_attachment=True)
-    # return processed_text
-
-# @app.route('/file-download/')
-# def file_download():
-#     return render_template('file.html')
-
-# @app.route('/return-file/')
-# def return_file():
-#     zipFolder = zipfile.ZipFile('videos.zip','w', zipfile.ZIP_DEFLATED)
-#     for root, directs, files in os.walk('./video'):
-# 		# if os.path.exists('./video/.DS_Store'):
-# 		# 	os.remove('./video/.DS_Store')
-#         for f in files:
-#             print(f)
-#             if str(f) == '.DS_Store':
-#                 continue
-#             zipFolder.write('./video/' + str(f))
-#     zipFolder.close()
-#     print('zip close')
-# 	# for root, directs, files in os.walk('./video'):
-# 	# 	for f in files:
-# 	# 		print(f)
-# 	# 		os.remove(f)
-#     os.system("rm video/*")
-#     return send_file('videos.zip', mimetype ='zip', attachment_filename = 'videos.zip', as_attachment=True)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    app.run(host="0.0.0.0", port=80)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
